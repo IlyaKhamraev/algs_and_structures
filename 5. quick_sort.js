0d8e3d46529f56ca15649a5d9079ad2624f722e9
Ilya Khamraev <hamraev9517@gmail.com>
2024-09-04 16:50:27 +0500

    added recursion

---
+++ 5. quick_sort.js
@@ -0,0 +1,12 @@
+// Быстрая сортировка
+
+const array = [
+    4, 0, 1, 11, 2, 17, -4, 3, 8, 20, 5, 18, 6, 12, 7, -2, 9, 10, 15, 14, 16,
+];
+let count = 0;
+
+function quickSort(arr) {}
+
+console.log(quickSort(array));
+console.log("array.length", array.length); // длинна массива
+console.log("count=", count); // количество операций
